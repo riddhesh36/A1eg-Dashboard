8564db3a91e1381193c5452b07d58f2295582da8
riddhesh36 <34064118+riddhesh36@users.noreply.github.com>
2025-11-08 01:52:43 +0530

    first commit4

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,13 @@ import TodoList from "@/components/TodoList";
 
 export default function Home() {
   return (
-    <div className="max-w-[1600px] mx-auto">
-      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
-          <div className="bg-primary-foreground p-4 rounded-md lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppBarCharts /></div>
-          <div className="bg-primary-foreground p-4 rounded-md"><CardList title="Latest Transactions"/></div>
-          <div className="bg-primary-foreground p-4 rounded-md"><AppPieChart /></div>
-          <div className="bg-primary-foreground p-4 rounded-md"><TodoList /></div>
-          <div className="bg-primary-foreground p-4 rounded-md lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppRadialChart /></div>
-          <div className="bg-primary-foreground p-4 rounded-md"><CardList title="Top Payer" /></div>
-      </div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+        <div className="bg-primary-foreground p-4 rounded-md lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppBarCharts /></div>
+        <div className="bg-primary-foreground p-4 rounded-md"><CardList title="Latest Transactions"/></div>
+        <div className="bg-primary-foreground p-4 rounded-md"><AppPieChart /></div>
+        <div className="bg-primary-foreground p-4 rounded-md"><TodoList /></div>
+        <div className="bg-primary-foreground p-4 rounded-md lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppRadialChart /></div>
+        <div className="bg-primary-foreground p-4 rounded-md"><CardList title="Top Payer" /></div>
     </div>
   );
 }
